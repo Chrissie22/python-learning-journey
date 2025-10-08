19968e6b21b417e8a1a4a51304de956c9fe7f3f5
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-08 20:34:09 +0100

    Add operators and variables practice scripts

---
+++ 01_python_basics/scripts/operators.py
@@ -0,0 +1,26 @@
+# Let's start with two numbers
+
+num1 = 20
+num2 = 5
+
+# -- Arithmetic Operators ---
+
+sum_result = num1 + num2
+product_result = num1 * num2
+division_result = num1 / num2
+
+print(f"{num1} + {num2} = {sum_result}")
+print(f"{num1} * {num2} = {product_result}")
+print(f"{num1} / {num2} = {division_result}")
+
+print("___") # This is just a seperator
+
+# --- Comparison Operators ---
+
+# The result of a comparison is always True or False
+
+are_they_equal = (num1 == num2)
+is_num1_bigger = (num1 > num2)
+
+print(f"Is {num1} equal to {num2}? {are_they_equal}")
+print(f"Is {num1} bigger than {num2}? {is_num1_bigger}")
---
+++ 01_python_basics/scripts/variables.py
@@ -0,0 +1,16 @@
+# A string variable
+user_name  = "Christabel"
+
+#An integer variables
+
+user_age = 12
+
+# A boolean variable
+
+is_learning_python = True
+
+# We can print them using an "f-string" to combine the text and variables
+
+print(f"My name is {user_name}.")
+print(f"I am {user_age} years old.")
+print(f"Am i learning Python? That is {is_learning_python}.")
