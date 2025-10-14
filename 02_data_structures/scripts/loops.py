ff1653a4ee93ebf9a31f52db09243da9f1c37555
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-14 13:22:42 +0100

    Added loops exercise script

---
+++ 02_data_structures/scripts/loops.py
@@ -0,0 +1,12 @@
+friends = ["Mary", "Elizabeth", "David"]
+
+print(" --- Sending a greeting to everyone ---")
+
+#This loop will visit each name in the list for 
+
+for friend in friends:
+    #The 'friend' variable will hold the name
+    print(f"Hello, {friend}! Have a great day. ")
+    
+print("\n--- All messages sent! ---")
+
