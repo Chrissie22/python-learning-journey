df682b1e1a0024c2d40a27a4fa4e5502bc3874ae
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-15 23:25:25 +0100

    Update grade filter exercise with improved logic and readability

--- 04_functions/exercises/exercise_grade_filter.py
+++ 04_functions/exercises/exercise_grade_filter.py
@@ -1,1 +1,13 @@
-exercise_grade_filter.py
+def filter_passing_scores(scores, pass_mark):
+    passing_scores = []
+    for score in scores:
+        if score >= pass_mark:
+            passing_scores.append(score)   
+    return passing_scores
+
+
+student_scores = [88, 45, 92, 71, 59, 60 ,99]
+
+passed_students = filter_passing_scores(student_scores, 70)
+
+print(passed_students)
