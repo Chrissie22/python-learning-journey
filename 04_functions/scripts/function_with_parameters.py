04ad7d49f436209fd7abd874bbf7529ad87d0fbd
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-15 13:01:43 +0100

    Add script demonstrating use of parameters in functions

---
+++ 04_functions/scripts/function_with_parameters.py
@@ -0,0 +1,15 @@
+# "name" is the PARAMETER. it's a placeholder
+
+def greet(name):
+    print(f"Hello, {name}!")
+    print(f"Welcome to functions, {name}.")
+    
+# --- Calling the function with ARGUMENTS ---
+
+# "Christabel" is the ARGUMENT we pass to the 'name' parameter.
+
+print ("--- Greeting Christabel ---")
+greet("Christabel")
+
+print("\n--- Greeting David ---")
+greet("David")
