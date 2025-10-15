67360557519a0b218cee05bc7814a24df5c01971
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-15 11:45:12 +0100

    Initialize functions module with basic examples

---
+++ 04_functions/scripts/first_function.py
@@ -0,0 +1,13 @@
+# --- Step 1: DEFINE the function ---
+# This part is like writing the recipe. The code inside doesn't run yet.
+
+def greet_user():
+    print("Hello, Christabel!")
+    print("Welcome to your first function")
+    
+# --- Step 2: CALL the function ---
+# Now that Python knows the recipe, we can "call" it to run the code.
+
+print("The program starts here.")
+greet_user()
+print("The program is finisheed")
