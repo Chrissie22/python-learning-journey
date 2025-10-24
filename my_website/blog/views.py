ca1b1d3f8caf399d0c2adbf092809d9079779f4f
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-24 23:53:24 +0100

    Add views for blog post list and detail pages

--- my_website/blog/views.py
+++ my_website/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    #1. Get all Post objects from the database and order them
+    all_posts = Post.objects.all().order_by('-id')
+    
+    # 2. Send them to a template called 'post_list.html'
+    # We'll create this template in the next step.
+    return render(request, 'blog/post_list.html', {'posts': all_posts})
