33efcf4c2bfac423f2177c5bc829914c51cc424c
Chrissie22 <christabel.ojobolo@gmail.com>
2025-10-24 23:43:05 +0100

    Register Blog models in admin panel

--- my_website/blog/admin.py
+++ my_website/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post # Import your Post model
 
 # Register your models here.
+# This tells Django to show the Post model in the admin site
+admin.site.register(Post) 
+
